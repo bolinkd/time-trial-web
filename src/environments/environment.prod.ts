9aeacd0a48306248ec6acb0aeb7e5336287ce3bb
Dustin Bolink <williambolink@gmail.com>
2019-02-14 11:24:39 -0800

    Added Production Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import {Environment} from './environment.interface';
 
 export const environment: Environment = {
   production: true,
-  api_root: 'https://floating-brushlands-72415.herokuapp.com/v1'
+  api_root: 'https://api-time-trial-vcrc.herokuapp.com/v1'
 };
